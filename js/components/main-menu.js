e27061b53768ee9c76effee7d697551b4454a605
shatalinmisha <Shatalin-1991@list.ru>
2025-06-02 15:43:35 +0300

    refactor
    Добавление новых функций

---
+++ js/components/main-menu.js
@@ -0,0 +1,11 @@
+
+// function getMainMenuActive(classList) {
+//     const mainMenuEl = document.querySelector('#mainMenu');
+//     mainMenuEl.classList.add(classList);
+//     return mainMenuEl;
+// }
+
+
+// export {
+//     getMainMenuActive
+// }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// import * as components from "./components/main-menu.js";
+
 window.addEventListener('DOMContentLoaded', () => {
+
+    const catalogButtonEl = document.querySelector('#catalogButton');
+    catalogButtonEl.addEventListener('click', function () {
+        const mainMenuEl = document.querySelector('#mainMenu');
+        mainMenuEl.classList.add('main-menu--active');
+    });
+
+    const mainMenuCloseEl = document.querySelector('#mainMenuClose');
+    mainMenuCloseEl.addEventListener('click', function () {
+        const mainMenuEl = document.querySelector('#mainMenu');
+        mainMenuEl.classList.remove('main-menu--active');
+    });
+
+    const locationCityButtonEl = document.querySelector('#locationCityButton');
+    locationCityButtonEl.addEventListener('click', function () {
+        locationCityButtonEl.classList.toggle('location__city--active');
+    });
+
+    const locationSublistEl = document.querySelector('#locationSublist');
+    const locationCityNameEl = document.querySelector('#locationCityName');
+
+    locationSublistEl.addEventListener('click', (event) => {
+        if (event.target.classList.contains('location__sublink')) {
+            const cityText = event.target.textContent;
+            locationCityNameEl.textContent = cityText;
+            locationCityButtonEl.classList.toggle('location__city--active');
+        }
+    });
 
 });
